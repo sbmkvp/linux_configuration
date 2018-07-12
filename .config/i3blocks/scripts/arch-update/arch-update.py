2965b2e52ad028edb73fe575fdc8e62a079e1dd8
Balamurugan Soundararaj <sbm.kvp@gmail.com>
2018-07-12 18:34:22 +0100

    more i3blocks update

--- .config/i3blocks/scripts/arch-update/arch-update.py
+++ .config/i3blocks/scripts/arch-update/arch-update.py
@@ -48,7 +48,7 @@ def create_argparse():
 
 
 def get_updates():
-    output = check_output(['checkupdates']).decode('utf-8')
+    output = check_output(['pacman','-Qu']).decode('utf-8')
     if not output:
         return []
 
@@ -62,7 +62,7 @@ def get_updates():
 def get_aur_updates():
     output = ''
     try:
-        output = check_output(['yaourt', '-Qua']).decode('utf-8')
+        output = check_output(['aurman', '-Qua']).decode('utf-8')
     except subprocess.CalledProcessError as exc:
         # yaourt exits with 1 and no output if no updates are available.
         # we ignore this case and go on
